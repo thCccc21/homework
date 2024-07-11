f9b2099764de224889a24b8dca39c9eb4e51c424
Vincent YT Chang <vincent_yt_chang@trendmicro.com>
2024-07-11 22:50:29 +0800

    2024-07-11

--- hello.py
+++ hello.py
@@ -1,2 +1,44 @@
-print("hello world")
+import random
+
+##for n in range(4) :
+a1 = random.randrange(10)
+a2 = random.randrange(10)
+a3 = random.randrange(10)
+a4 = random.randrange(10)
+while a1 == a2:
+    a2 = random.randrange(10)
+    if a1 != a2:
+        break
+while a3 == a2:
+    a3 = random.randrange(10)
+    if a3 != a2:
+        break
+while a3 == a1:
+    a3 = random.randrange(10)
+    if a3 != a1:
+        break
+while a4 == a1:
+     a4 = random.randrange(10)
+     if a4 != a3:
+        break
+while a4 == a2:
+    a4 = random.randrange(10)
+    if a4 != a3:
+        break
+while a1 == a3:
+    a4 = random.randrange(10)
+    if a4 != a3:
+        break
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+
+
+
+
+
+
+
+
 
