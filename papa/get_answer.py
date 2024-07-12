36190007cf20e4144e1a8e94672bf154e41287b2
Vincent YT Chang <vincent_yt_chang@trendmicro.com>
2024-07-12 22:04:11 +0800

    24/07/12

--- papa/get_answer.py
+++ papa/get_answer.py
@@ -4,7 +4,7 @@ def get_answer():
     answer = ''
 
     while len(answer) < 4:
-        s = str(random.randint(0, 9))
+        s = str(random.randrange(9))
         if s in answer:
             print(f'duplicated number: {s}')
             continue
@@ -14,4 +14,20 @@ def get_answer():
 
 answer = get_answer()
 print(f'answer is {answer}')
+
+while True :
+    str = input("please input")
+    m = str.isdigit()
+    if str == 'quit' :
+        print('answer is '+ answer)
+        break
+    if m == False :
+        print('only number')
+        continue
+    if len(str) != 4 :
+        print("four Numbers")
+        continue
+   
+
+
 
