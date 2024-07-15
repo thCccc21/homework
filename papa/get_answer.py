92ae5894909950b63785661948920a06bc9d2273
Vincent YT Chang <vincent_yt_chang@trendmicro.com>
2024-07-15 21:48:41 +0800

    24/07/15

--- papa/get_answer.py
+++ papa/get_answer.py
@@ -6,17 +6,17 @@ def get_answer():
     while len(answer) < 4:
         s = str(random.randrange(9))
         if s in answer:
-            print(f'duplicated number: {s}')
+           ## print(f'duplicated number: {s}')
             continue
         answer += s
 
     return answer
 
 answer = get_answer()
-print(f'answer is {answer}')
+##print(f'answer is {answer}')
 
 while True :
-    str = input("please input")
+    str = input("please input : ")
     m = str.isdigit()
     if str == 'quit' :
         print('answer is '+ answer)
@@ -27,7 +27,25 @@ while True :
     if len(str) != 4 :
         print("four Numbers")
         continue
-   
 
+    for i in range(4):
+        if str[i] in str[i+1:4] :
+            print(f'duplicated number {str[i]}')
+            continue
 
+    count_A = 0
+    for i in range(4) :
+        if str[i] == answer[i] :
+            count_A += 1
+
+    if count_A == 4:
+        print('victory')
+        break
+
+    count_B = 0
+    for i in range(4) :
+        for j in range(4) :
+            if answer[i] == str[j] and i != j:
+                count_B += 1
+    print(f'hint {count_A}A,{count_B}B')
 
